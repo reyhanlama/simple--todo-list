1566a74bf1cfa764fcc9519fc3860632cb423132
reyhantamang@gmail.com <reyhantamang@gmail.com>
2021-07-16 12:00:35 +0530

    placeholder files

---
+++ src/TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const TodoItem = () => {
+  return (
+    <div className="todo-item">
+      <input type="checkbox" />
+      <p>placeholder text</p>
+    </div>
+  )
+}
+
+export default TodoItem
