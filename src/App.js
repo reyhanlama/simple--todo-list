623fcb80f5f6c7e188127adb5aefc51ed2f2e0b8
reyhantamang@gmail.com <reyhantamang@gmail.com>
2021-07-17 13:58:20 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from "react"
+import React, { Component } from "react"
 import TodoItem from "./TodoItem"
-function App() {
-  return (
-    <div className="todo-list">
-      <h1>To do List</h1>
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-    </div>
-  )
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todo: [],
+    }
+  }
+  render() {
+    return (
+      <div className="todo-list">
+        <h1>To do List</h1>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
+    )
+  }
 }
 
 export default App
